3cd247bc06abef61fb8c2a063c1c5b827df5f096
William Del Giudice <delwillgiudice@gmail.com>
2021-03-24 17:03:10 -0400

    wile lup and listner are blocking, import keyboard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import DEFAULT_METER_ORIENTATION
 import pyautogui as bot
-import pynput
+from pynput.mouse import Listener as mouse
+from pynput.keyboard import Key, Listener as keyboard
 
 # bot.PAUSE = 0.1
 
@@ -10,10 +12,29 @@ import pynput
 #     bot.press("left")
 
 
+def clickHandler(arg1, arg2, arg3, arg4):
+    0
+
+
+def keyPressHandler(key):
+    key
+    print(key)
+
+
+def keyReleaseHandler(key):
+    print(key)
+
+
 layout = [[sg.Button("Record"), sg.Button("Playback"), sg.Input("1", key="iterations", size=(10, 10))]]
 window = sg.Window(title="Micrsoft Office Generator", layout=layout, margins=(20, 20))
 recording = False
 while True:
+    with mouse(on_click=clickHandler) as listener:
+        listener.join()
+
+    with keyboard(on_press=keyPressHandler, on_release=keyReleaseHandler) as listener:
+        listener.join()
+
     event, values = window.read()
 
     if event == "Record" and values["iterations"]:
